5b199c48b817544ebf76a640d7e5fa222f19719f
Morgan Stair <morgan@tadmuck.com>
2025-10-16 18:16:00 -0400

    feat: include __version__ in module exports

--- src/mstair/common/__init__.py
+++ src/mstair/common/__init__.py
@@ -1,8 +1,13 @@
 # File: python/plib_/__init__.py
 
+from __future__ import annotations
+
 from . import base, format_helpers, tokenize_helpers, xdumps, xlogging
 
 
-__all__ = ["base", "format_helpers", "tokenize_helpers", "xdumps", "xlogging"]
+__all__ = ["base", "format_helpers", "tokenize_helpers", "xdumps", "xlogging", "__version__"]
+
+__version__: str = "0.1.4"
+
 
 # End of file: src/mstair/common/__init__.py
